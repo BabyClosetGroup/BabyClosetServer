a3d11bced2d7ecd5a6b706b7a09fbf3107d56744
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-08-29 22:06:08 +0900

    change project structure

--- babyCloset/api/controllers/postController.js
+++ babyCloset/api/controllers/postController.js
@@ -1,7 +1,7 @@
 const resForm = require('../../modules/utils/rest/responseForm');
 const statusCode = require('../../modules/utils/rest/statusCode');
 const resMessage = require('../../modules/utils/rest/responseMessage');
-const db = require('../../modules/utils/db/pool');
+const postDAO = require('../dataAcessObjects/postDAO');
 const moment = require('moment');
 
 module.exports = {
@@ -24,36 +24,13 @@ module.exports = {
         else
         {
             deadline = moment().add(req.body.deadline.substring(0,1), 'days').format('YYYY-MM-DD');
-            const insertPostQuery = 'INSERT INTO post (postTitle, postContent, deadline, createdTime, userIdx)' +
-            ' VALUES (?, ?, ?, ?, ?)';
-            const insertPostImageQuery = 'INSERT INTO postImage (postImage, postIdx) VALUES (?, ?)';
-            const insertAreaCategoryQuery = 'INSERT INTO areaCategory (areaName) VALUES (?)';
-            const insertAgeCategoryQuery = 'INSERT INTO ageCategory (ageName) VALUES (?)';
-            const insertClothCategoryQuery = 'INSERT INTO clothCategory (clothName) VALUES (?)';
-            const insertPostAreaCategoryQuery = 'INSERT INTO postAreaCategory (postIdx, areaCategoryIdx) VALUES (?, ?)';
-            const insertPostAgeCategoryQuery = 'INSERT INTO postAgeCategory (postIdx, ageCategoryIdx) VALUES (?, ?)';
-            const insertPostClothCategoryQuery = 'INSERT INTO postClothCategory (postIdx, clothCategoryIdx) VALUES (?, ?)';
-            const insertTransaction = await db.Transaction(async(connection) => {
-                const insertPostResult = await connection.query(insertPostQuery, [postTitle, postContent, deadline, createdTime, userIdx]);
-                const postIdx = insertPostResult.insertId;
-                for(i=0; i<postImages.length ;i++)
-                    await connection.query(insertPostImageQuery, [postImages[i].location, postIdx]);
-                const insertAreaCategoryResult = await connection.query(insertAreaCategoryQuery, [areaName]);
-                const insertAgeCategoryResult = await connection.query(insertAgeCategoryQuery, [ageName]);
-                const insertClothCategoryResult = await connection.query(insertClothCategoryQuery, [clothName]);
-                const areaCategoryIdx = insertAreaCategoryResult.insertId;
-                const ageCategoryIdx = insertAgeCategoryResult.insertId;
-                const clothCategoryIdx = insertClothCategoryResult.insertId;
-                await connection.query(insertPostAreaCategoryQuery, [postIdx, areaCategoryIdx]);
-                await connection.query(insertPostAgeCategoryQuery, [postIdx, ageCategoryIdx]);
-                await connection.query(insertPostClothCategoryQuery, [postIdx, clothCategoryIdx]);
-                });
+            const insertTransaction = await postDAO.RegisterPost(postImages, postTitle, postContent, deadline, createdTime, userIdx, areaName, ageName, clothName)
             if (!insertTransaction) {
                 res.status(200).send(resForm.successFalse(statusCode.DB_ERROR, resMessage.FAIL_CREATED_X('게시물')));
                 } else {
                 res.status(200).send(resForm.successTrue(statusCode.OK, resMessage.CREATED_X('게시물')));
             }
         }
-    }
+    },
+        // 받아야 할 정보 이미지(여러 개), 카테고리(자치구, 나이, 옷 종류), 마감기한, 제목, 내용 
 }
-// 받아야 할 정보 이미지(여러 개), 카테고리(자치구, 나이, 옷 종류), 마감기한, 제목, 내용 
---
+++ babyCloset/api/dataAcessObjects/postDAO.js
@@ -0,0 +1,31 @@
+const db = require('../../modules/utils/db/pool');
+
+module.exports = {
+    RegisterPost : async (postImages, postTitle, postContent, deadline, createdTime, userIdx, areaName, ageName, clothName)  => {
+        const insertPostQuery = 'INSERT INTO post (postTitle, postContent, deadline, createdTime, userIdx)' +
+            ' VALUES (?, ?, ?, ?, ?)';
+            const insertPostImageQuery = 'INSERT INTO postImage (postImage, postIdx) VALUES (?, ?)';
+            const insertAreaCategoryQuery = 'INSERT INTO areaCategory (areaName) VALUES (?)';
+            const insertAgeCategoryQuery = 'INSERT INTO ageCategory (ageName) VALUES (?)';
+            const insertClothCategoryQuery = 'INSERT INTO clothCategory (clothName) VALUES (?)';
+            const insertPostAreaCategoryQuery = 'INSERT INTO postAreaCategory (postIdx, areaCategoryIdx) VALUES (?, ?)';
+            const insertPostAgeCategoryQuery = 'INSERT INTO postAgeCategory (postIdx, ageCategoryIdx) VALUES (?, ?)';
+            const insertPostClothCategoryQuery = 'INSERT INTO postClothCategory (postIdx, clothCategoryIdx) VALUES (?, ?)';
+            const insertTransaction = await db.Transaction(async(connection) => {
+                const insertPostResult = await connection.query(insertPostQuery, [postTitle, postContent, deadline, createdTime, userIdx]);
+                const postIdx = insertPostResult.insertId;
+                for(i=0; i<postImages.length ;i++)
+                    await connection.query(insertPostImageQuery, [postImages[i].location, postIdx]);
+                const insertAreaCategoryResult = await connection.query(insertAreaCategoryQuery, [areaName]);
+                const insertAgeCategoryResult = await connection.query(insertAgeCategoryQuery, [ageName]);
+                const insertClothCategoryResult = await connection.query(insertClothCategoryQuery, [clothName]);
+                const areaCategoryIdx = insertAreaCategoryResult.insertId;
+                const ageCategoryIdx = insertAgeCategoryResult.insertId;
+                const clothCategoryIdx = insertClothCategoryResult.insertId;
+                await connection.query(insertPostAreaCategoryQuery, [postIdx, areaCategoryIdx]);
+                await connection.query(insertPostAgeCategoryQuery, [postIdx, ageCategoryIdx]);
+                await connection.query(insertPostClothCategoryQuery, [postIdx, clothCategoryIdx]);
+                });
+            return insertTransaction;
+    }
+}
