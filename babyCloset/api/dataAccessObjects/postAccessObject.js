fbdef3ba82186e42fe7c710d4e5675bd4086d2aa
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-08-31 21:48:43 +0900

    make deadline post query tunning

--- babyCloset/api/dataAccessObjects/postAccessObject.js
+++ babyCloset/api/dataAccessObjects/postAccessObject.js
@@ -33,19 +33,16 @@ module.exports = {
     GetDeadLinePost : async () => {
         const selectDeadlinePostQuery = `
         SELECT
-        postAreaImage.postIdx, postAreaImage.postTitle, postAreaImage.deadline, postAreaImage.postImage, areaCategory.areaName
+        postAreaImage.postIdx, postAreaImage.postTitle, postAreaImage.deadline, postAreaImage.mainImage, areaCategory.areaName
         FROM areaCategory
         JOIN
-        (SELECT postArea.postIdx, postArea.postTitle, postArea.deadline, postImage.postImage, postArea.areaCategoryIdx
-        FROM postImage
-        JOIN
-        (SELECT post.postIdx, post.postTitle, post.deadline, postAreaCategory.areaCategoryIdx
+        (SELECT postArea.postIdx, postArea.postTitle, postArea.deadline, postArea.mainImage, postArea.areaCategoryIdx
+        FROM 
+        (SELECT post.postIdx, post.postTitle, post.deadline, post.mainImage, postAreaCategory.areaCategoryIdx
         FROM postAreaCategory
         JOIN post ON postAreaCategory.postIdx = post.postIdx
         WHERE post.deadline <= curdate() + interval 4 day AND post.deadline > curdate() - interval 1 day)
-        AS postArea
-        ON postArea.postIdx = postImage.postIdx
-        GROUP BY postArea.postIdx)
+        AS postArea)
         AS postAreaImage
         ON postAreaImage.areaCategoryIdx = areaCategory.areaCategoryIdx
         ORDER BY deadline LIMIT 3`
