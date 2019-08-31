d2840eca657e5bcff25ff340f94327d054dea68b
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-08-31 22:03:41 +0900

    make deadline post with pagination query tunning

--- babyCloset/api/dataAccessObjects/postAccessObject.js
+++ babyCloset/api/dataAccessObjects/postAccessObject.js
@@ -88,20 +88,17 @@ module.exports = {
     GetDeadLinePostWithPagination : async (offset) => {
         const selectDeadlinePostQuery = `
         SELECT
-        postAreaImage.postIdx, postAreaImage.postTitle, postAreaImage.deadline, postAreaImage.postImage, areaCategory.areaName
+        postAreaImage.postIdx, postAreaImage.postTitle, postAreaImage.deadline, postAreaImage.mainImage, areaCategory.areaName
         FROM areaCategory
         JOIN
-        (SELECT postArea.postIdx, postArea.postTitle, postArea.deadline, postImage.postImage, postArea.areaCategoryIdx
-        FROM postImage
-        JOIN
-        (SELECT post.postIdx, post.postTitle, post.deadline, postAreaCategory.areaCategoryIdx
+        (SELECT postArea.postIdx, postArea.postTitle, postArea.deadline, postArea.mainImage, postArea.areaCategoryIdx
+        FROM
+        (SELECT post.postIdx, post.postTitle, post.deadline, post.mainImage, postAreaCategory.areaCategoryIdx
         FROM postAreaCategory
         JOIN post
         ON postAreaCategory.postIdx = post.postIdx
         WHERE post.deadline <= curdate() + interval 4 day AND post.deadline > curdate() - interval 1 day)
-        AS postArea
-        ON postArea.postIdx = postImage.postIdx
-        GROUP BY postArea.postIdx)
+        AS postArea)
         AS postAreaImage
         ON postAreaImage.areaCategoryIdx = areaCategory.areaCategoryIdx
         ORDER BY deadline LIMIT 8 OFFSET ${offset}`
