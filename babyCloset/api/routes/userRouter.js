2bee6cf070cf3c62096b18d29cd840b5e4e36bbd
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-09-01 12:54:21 +0900

    change http method of updating profile api

--- babyCloset/api/routes/userRouter.js
+++ babyCloset/api/routes/userRouter.js
@@ -6,6 +6,6 @@ const authUtil = require('../../modules/utils/security/authUtils');
 
 router.post('/signup', UserController.SignUp);
 router.post('/signin', UserController.SignIn);
-router.post('/', upload.single('profileImage'), authUtil.isLoggedIn, UserController.UpdateProfile);
+router.put('/', upload.single('profileImage'), authUtil.isLoggedIn, UserController.UpdateProfile);
 
 module.exports = router;
