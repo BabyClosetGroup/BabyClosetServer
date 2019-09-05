da7f7b84b79d799cf2c26adb15aa20cd25781a5c
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-09-05 21:30:44 +0900

    post rating sql tunning

--- babyCloset/api/dataAccessObjects/ratingAccessObject.js
+++ babyCloset/api/dataAccessObjects/ratingAccessObject.js
@@ -2,11 +2,11 @@ const db = require('../../modules/utils/db/pool');
 
 module.exports = {
     PostRating : async (userIdx, rating)  => {
-        const updateRatingQuery = `UPDATE user SET rating = IF (ratingCount = 0, ?, (rating * ratingCount + ?)/(ratingCount+1))
+        const updateRatingQuery = `UPDATE user SET rating = (rating * ratingCount + ?)/(ratingCount+1)
         WHERE userIdx = ?`
         const updateRatingCountQuery = 'UPDATE user SET ratingCount = ratingCount + 1 where userIdx = ?';
         const updateTransaction = await db.Transaction(async(connection) => {
-            await connection.query(updateRatingQuery, [rating, rating, userIdx]);
+            await connection.query(updateRatingQuery, [rating, userIdx]);
             await connection.query(updateRatingCountQuery, [userIdx]);
         });
         return updateTransaction;
