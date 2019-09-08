cfc0b685176a8fe3c703cbd4c7644216a4ebac21
HwangJaeSuk <jaesukhwang95@gmail.com>
2019-09-08 14:15:57 +0900

    get applicant query

--- babyCloset/api/dataAccessObjects/shareAccessObject.js
+++ babyCloset/api/dataAccessObjects/shareAccessObject.js
@@ -20,4 +20,21 @@ module.exports = {
         const selectPostResult = db.queryParam_Arr(selectPostQuery, [userIdx]);
         return selectPostResult;
     },
+    GetDetailUncompletedShare : async(userIdx, postIdx) => {
+        const selectPostQuery = 
+        `SELECT selectedPost.postIdx, selectedPost.postTitle, selectedPost.mainImage, area.areaName, selectedPost.registerNumber FROM
+        (SELECT postIdx, postTitle, mainImage, registerNumber FROM post WHERE isShared = 0 AND userIdx = ? AND postIdx = ?)  AS selectedPost,
+        (SELECT postAreaCategory.postIdx, areaCategory.areaName
+        FROM areaCategory, postAreaCategory WHERE areaCategory.areaCategoryIdx = postAreaCategory.areaCategoryIdx) AS area
+        WHERE selectedPost.postIdx = area.postIdx`;
+        const selectPostResult = db.queryParam_Arr(selectPostQuery, [userIdx, postIdx]);
+        return selectPostResult;
+    },
+    GetApplicantInformation : async(postIdx) => {
+        const selectApplicantQuery = `
+        SELECT a.receiverIdx AS applicantIdx, b.nickname AS applicantNickname, b.rating FROM sharingWant
+        AS a JOIN  user AS b WHERE a.postIdx = ? AND a.receiverIdx = b.userIdx`
+        const selectApplicantResult = db.queryParam_Arr(selectApplicantQuery, [postIdx]);
+        return selectApplicantResult;
+    }
 }
